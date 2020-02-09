036784c570623c59414ae5c0963d326d886932f7
Murad Hr <49716101+jedan2@users.noreply.github.com>
2020-02-09 17:00:38 +0600

    Add files via upload

---
+++ resource/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+
+//sticky
+	$(".js--services-section").waypoint(function(direction){
+		if (direction == "down") {
+				$("nav").addClass("sticky");
+		} else{
+			$("nav").removeClass("sticky");
+		}
+
+	})
+
+
+	//mixitup (portfolio)
+	var mixer = mixitup('.container');
+});
